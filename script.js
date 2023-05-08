baf01fb35a9ceb7122f4660c682d139d0181e409
reginalimaa <mariardlima@fac.pe.senac.br>
2023-05-08 20:41:30 -0300

    tratanto erros 1

---
+++ script.js
@@ -0,0 +1,12 @@
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error("Esse cep não existe")
+        }else
+        console.log(r)
+    })
+    .catch(erro => console.logo(erro))
+    .finally(mensagem => console.log("Processamento concluído!"));
+
+console.log(consultaCep);
